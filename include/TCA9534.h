498aa52ed322c97b09f5e7b28896c5c7dd190886
Victor Hogeweij <vo.hogeweij@student.han.nl>
2022-11-28 23:19:27 +0100

    -add small improvements before making it completely cpp

--- include/TCA9534.h
+++ include/TCA9534.h
@@ -2,6 +2,7 @@
 #define TCA9534_IDF_TCA9534_H
 
 #include <driver/i2c.h>
+#include <driver/gpio.h>
 
 #define TCA9534_ERROR -1
 
@@ -34,7 +35,18 @@ typedef struct {
     uint8_t I2C_ADDR;
     i2c_config_t i2c_conf;
     int i2c_master_port;
+    //Only when mode is set to interrupt, otherwise it won't be used..
+    gpio_num_t interrupt_pin;
+    TaskHandle_t* interrupt_task;
 } TCA9534_IO_EXP;
+
+/**
+ * @brief Setup interrupts using the builtin IO_EXP_INT pin of the tca9534 and interrupt handler+task
+ * @param io_exp which contains the gpio pin where IO_EXP_INT is connected(interrupt_pin)
+ *               And optionally contains the task to run when interrupt triggered (interrupt_task) if not defined the
+ *               default handler will be used.
+ */
+void setup_tca9534_interrupt_handler(TCA9534_IO_EXP* io_exp);
 
 /**
  * @brief Get the current input state of the specified input pin (1 or 0)
@@ -47,7 +59,7 @@ typedef struct {
  *     - 1   Success! Pin is High
  *     - TCA9534_ERROR(-1)  Error!   Something went wrong in the process of reading the io expander
  */
-int16_t get_io_pin_input_status(TCA9534_IO_EXP io_exp, TCA9534_PINS io_pin);
+int16_t get_io_pin_input_status(TCA9534_IO_EXP* io_exp, TCA9534_PINS io_pin);
 
 /**
  * @brief Get the current input state of all the io expander pins
@@ -58,7 +70,7 @@ int16_t get_io_pin_input_status(TCA9534_IO_EXP io_exp, TCA9534_PINS io_pin);
  *     - 0x00 - 0xFF  Success! Dump of input register, 1 bit is equal to 1 of the physical pins (Lower 8 bits of 16 bits result)
  *     - TCA9534_ERROR(-1)  Error!   Something went wrong in the process of reading the io expander
  */
-int16_t get_tca9534_all_io_pin_input_status(TCA9534_IO_EXP io_exp);
+int16_t get_tca9534_all_io_pin_input_status(TCA9534_IO_EXP* io_exp);
 
 /**
  * @brief Get the current direction of all the io expander pins
@@ -69,7 +81,7 @@ int16_t get_tca9534_all_io_pin_input_status(TCA9534_IO_EXP io_exp);
  *     - 0x00 - 0xFF  Success! Dump of configuration register, 1 bit is equal to 1 of the physical pins (Lower 8 bits of 16 bits result)
  *     - TCA9534_ERROR(-1)  Error!   Something went wrong in the process of reading the io expander
  */
-int16_t get_all_io_pin_direction(TCA9534_IO_EXP io_exp);
+int16_t get_all_io_pin_direction(TCA9534_IO_EXP* io_exp);
 
 /**
  * @brief Get the current polarity inversion state of all the io expander pins
@@ -80,7 +92,7 @@ int16_t get_all_io_pin_direction(TCA9534_IO_EXP io_exp);
  *     - 0x00 - 0xFF  Success! Dump of configuration register, 1 bit is equal to 1 of the physical pins (Lower 8 bits of 16 bits result)
  *     - TCA9534_ERROR(-1)  Error!   Something went wrong in the process of reading the io expander
  */
-int16_t get_all_io_polarity_inversion(TCA9534_IO_EXP io_exp);
+int16_t get_all_io_polarity_inversion(TCA9534_IO_EXP* io_exp);
 
 /**
  * @brief Get the current direction of the specified io expander pin
@@ -93,7 +105,7 @@ int16_t get_all_io_polarity_inversion(TCA9534_IO_EXP io_exp);
  *     - 1   Success! Pin is Inverted
  *     - TCA9534_ERROR(-1)  Error!   Something went wrong in the process of reading the io expander
  */
-int16_t get_io_pin_polarity_inversion(TCA9534_IO_EXP io_exp, TCA9534_PINS io_pin);
+int16_t get_io_pin_polarity_inversion(TCA9534_IO_EXP* io_exp, TCA9534_PINS io_pin);
 
 /**
  * @brief Get the current direction of the specified physical pin (0 means OUTPUT or 1 means INPUT)
@@ -106,7 +118,7 @@ int16_t get_io_pin_polarity_inversion(TCA9534_IO_EXP io_exp, TCA9534_PINS io_pin
  *     - 1   Success! Pin is INPUT
  *     - TCA9534_ERROR(-1)  Error!   Something went wrong in the process of reading the io expander
  */
-int16_t get_io_pin_direction(TCA9534_IO_EXP io_exp, TCA9534_PINS io_pin);
+int16_t get_io_pin_direction(TCA9534_IO_EXP* io_exp, TCA9534_PINS io_pin);
 
 /**
  * @brief Sets all physical pins of the io expander to a specified direction (INPUT or OUTPUT)
@@ -118,7 +130,7 @@ int16_t get_io_pin_direction(TCA9534_IO_EXP io_exp, TCA9534_PINS io_pin);
  *     - ESP_OK  Success!
  *     - ESP_ERR Error!
  */
-esp_err_t set_all_tca9534_io_pins_direction(TCA9534_IO_EXP io_exp, TCA9534_PORT_DIRECTION properties);
+esp_err_t set_all_tca9534_io_pins_direction(TCA9534_IO_EXP* io_exp, TCA9534_PORT_DIRECTION properties);
 
 /**
  * @brief Set physical pin of the io expander to a specified direction (INPUT or OUTPUT)
@@ -131,7 +143,7 @@ esp_err_t set_all_tca9534_io_pins_direction(TCA9534_IO_EXP io_exp, TCA9534_PORT_
  *     - ESP_OK  Success!
  *     - ESP_ERR Error!
  */
-esp_err_t set_tca9534_io_pin_direction(TCA9534_IO_EXP io_exp, TCA9534_PINS io_pin, TCA9534_PORT_DIRECTION properties);
+esp_err_t set_tca9534_io_pin_direction(TCA9534_IO_EXP* io_exp, TCA9534_PINS io_pin, TCA9534_PORT_DIRECTION properties);
 
 /**
  * @brief Set physical pin of the io expander to an specified output state (HIGH(1) or LOW(0))
@@ -146,7 +158,6 @@ esp_err_t set_tca9534_io_pin_direction(TCA9534_IO_EXP io_exp, TCA9534_PINS io_pi
  *
  * @note Pin output state can be inverted with the inversion register
  */
-esp_err_t set_tca9534_io_pin_output_state(TCA9534_IO_EXP io_exp, TCA9534_PINS io_pin, uint8_t state);
-
+esp_err_t set_tca9534_io_pin_output_state(TCA9534_IO_EXP* io_exp, TCA9534_PINS io_pin, uint8_t state);
 
 #endif //TCA9534_IDF_TCA9534_H
